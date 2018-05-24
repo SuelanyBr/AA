712bada343803c66fa68958f3039311c0987ca67
Suelany Brito <suelanybr@gmail.com>
2018-05-23 22:53:25 -0300

    ..

--- URI/2562.cpp
+++ URI/2562.cpp
@@ -2,74 +2,67 @@
 #include <list>
 #include <algorithm>
 #define MAX 1000
+#define ll long long int
 using namespace std;
 
 class Graph{
-    int numVertices;
-    list<int> *adj;
+    ll numVertices;
+    list<ll> *adj;
     
 public:
-    Graph(int V);
-    void addAresta(int V1, int V2);
-	void addVizinhos(Graph g, int v1, int v2);
-	int sizeList(int V1);
-	void printList();
+    Graph(ll V);
+    void addAresta(ll V1, ll V2);
+	void addVizinhos(ll v1, ll v2);
+	int sizeList(ll V1);
+
 };
  
-Graph::Graph(int vertices){
+Graph::Graph(ll vertices){
     numVertices = vertices;
-    adj = new list<int>[vertices];
+    adj = new list<ll>[vertices];
 }
  
-void Graph::addAresta(int V1, int V2){
+void Graph::addAresta(ll V1, ll V2){
     adj[V1].push_front(V2);
+	adj[V2].push_front(V1);
 }
 
-void Graph::addVizinhos(Graph g,int v1, int v2){
-	cout << "adj[v1]: " << endl; 
-	list<int>::iterator it2 = find(adj[v1].begin(), adj[v1].end(), v2);
-	
-	if(it2 == adj[v1].end()){
-		for (std::list<int>::iterator it = adj[v1].begin(); it != adj[v1].end(); it++){
-			g.addAresta(*it,v2);
-			cout << *it << endl; 
+void Graph::addVizinhos(ll v1, ll v2){
+	for (list<ll>::iterator it = adj[v1].begin(); it != adj[v1].end(); it++){
+		if(*it != v2){
+			ll x = *it;
+			adj[x].push_front(v2);
 		}
+			
 	}
-	
 }
 
-int Graph::sizeList(int V1){
+int Graph::sizeList(ll V1){
     return adj[V1].size();
 }
 
-void Graph::printList(){
-	cout << "Print List: " << endl;
-	
-	cout << adj << endl; 
-	
-}
- 
  
 int main(){
-	int N,M;
-	int V1,V2;
-	int E;
+	ll N,M;
+	ll V1,V2;
+	ll E;
 	while(cin >> N >> M){
 		
-		Graph g(MAX);
-		for(int i = 0; i < M; i++){
+		Graph g(N);
+		for(ll i = 0; i < M; i++){
 			cin >> V1 >> V2;
+			
 			g.addAresta(V1, V2);
-			g.addAresta(V2, V1);
-			g.addVizinhos(g,V1,V2);
-			g.addVizinhos(g,V2,V1);
+			
+			g.addVizinhos(V1,V2);
+			g.addVizinhos(V2,V1);
 		}
 		
 	
 		cin >> E;
 		
-		int x = g.sizeList(E);
-		cout << x << endl;
+		ll x = g.sizeList(E);
+		cout << x+1 << endl;
 		
 	}
     return 0;
