3a3fe42a45a7b8f7e0b96c80b23d7340ca6bd206
SuelanyBr <maria.cruz@ccc.ufcg.edu.br>
2018-07-10 17:28:27 -0300

    Lista 9 - DP

--- URI/1286.cpp
+++ URI/1286.cpp
@@ -1,21 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long dp[50][50];
+int tempo[200], qtd_pizza[200];
+
+long long go(int n, int i, int m) {
+	if (n == i) return 0;
+	
+	if (dp[i][m] != -1) return dp[i][m];
+	
+	long long best = go(n, i + 1, m);
+	
+	if (m >= qtd_pizza[i]) {
+		best = max(best, go(n, i + 1, m - qtd_pizza[i]) + tempo[i]);
+	}
+	
+	return dp[i][m] = best;
+}
+
 int main(){
-	int N, num, num_1, maxPizza;
+	int num_pedidos, maxPizza;
 	
-	
-	
+
 	while(true){
-		cin >> N;
-		if(N == 0) break;
+		cin >> num_pedidos;
+		if(num_pedidos == 0) break;
 		
 		cin >> maxPizza;
-		for(int i = 0; i < N; i++){
-			cin >> num >> num_1;
+		
+		for(int i = 0; i < num_pedidos; i++){
+			
+			cin >> tempo[i] >> qtd_pizza[i];
 		}
-	
-		cout << maxValue << endl;
+		
+		memset(dp, -1, sizeof(dp));
+		long long best = go(num_pedidos, 0, maxPizza);
+		cout << best << " min." << endl;
 	}
 	
 	return 0;
